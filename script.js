a29a2410a713f031ac8feba221d64f48539e7c29
Mahmoud Ayman <m7m0udayman@gmail.com>
2022-07-25 19:07:15 +0200

    trying to make project work with local storage

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let addTaskBtn = document.getElementById("add_task_btn");
 let errMsg = document.getElementById("error_msg");
 let taskList = document.querySelector(".task_box");
 let i = 1;
+
+let tasks = JSON.parse(localStorage.getItem("taskList"));
+
 addTaskBtn.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (taskName.value == "") {
@@ -12,7 +15,7 @@ addTaskBtn.addEventListener("click", function (evt) {
     new_task.classList.add("task");
     new_task.id = `task_${i}`;
     new_task.innerHTML = `<label for="task${i}">
-      <input type="checkbox" name="" id="task${i}" />
+      <input type="checkbox" onclick="markAsComplete('1')" name="" id="task${i}" />
       <p>${taskName.value}</p>
     </label>
     <div class="task_controls" id="taskControls${i}">
@@ -32,22 +35,48 @@ addTaskBtn.addEventListener("click", function (evt) {
     i++;
   }
 });
+function showTodoList() {
+  if (tasks) {
+    tasks.forEach((element, i) => {
+      let new_task = document.createElement("li");
+      new_task.classList.add("task");
+      new_task.id = `task_${i}`;
+      new_task.innerHTML = `<label for="task${i}">
+      <input type="checkbox" onclick="markAsComplete('1')" name="" id="task${i}" />
+      <p>${taskName.value}</p>
+    </label>
+    <div class="task_controls" id="taskControls${i}">
+      <i class="fa-solid fa-ellipsis"></i>
+      <ul class="task_operations">
+        <li class="edit_task" onclick="edit_task('${i}')">
+          <i class="fa-solid fa-pen-clip"></i>Edit
+        </li>
+        <li class="delete_task" onclick="delete_task('${i}')">
+          <i class="fa-solid fa-trash"></i>Delete
+        </li>
+      </ul>
+    </div>`;
+      console.log(new_task);
+      taskList.appendChild(new_task);
+    });
+  }
+}
+showTodoList();
 function edit_task(id) {
   let task = document.getElementById(`task_${id}`);
   let taskName = task.firstChild.querySelector("p");
   task.firstChild.innerHTML = `<input type="text" id="task_name" value="${taskName.innerHTML}" /> <button class="save_task" onclick="save_task('${id}')">Save</button>`;
-    let task_ops= document.getElementById(`taskControls${id}`);
-    task_ops.style.display = "none";
+  let task_ops = document.getElementById(`taskControls${id}`);
+  task_ops.style.display = "none";
 }
 function save_task(id) {
   let task = document.getElementById(`task_${id}`);
   let taskName = task.firstChild.querySelector("input");
-  task.firstChild.innerHTML = `<label for="task${id}">
+  task.firstChild.innerHTML = `
       <input type="checkbox" name="" id="task${id}" />
-      <p>${taskName.value}</p>
-    </label>`;
-    let task_ops= document.getElementById(`taskControls${id}`);
-    task_ops.style.display = "block";
+      <p>${taskName.value}</p>`;
+  let task_ops = document.getElementById(`taskControls${id}`);
+  task_ops.style.display = "block";
 }
 function delete_task(id) {
   let task = document.getElementById(`task_${id}`);
@@ -56,3 +85,15 @@ function delete_task(id) {
 document.querySelector(".clear_btn").addEventListener("click", function () {
   taskList.innerHTML = "";
 });
+completedTasks = [];
+function markAsComplete(id) {
+  let task = document.getElementById(`task_${id}`);
+  if (task.querySelector("input").checked) {
+    task.querySelector("p").classList.add("completed");
+    tasks[id].status = "true";
+  } else {
+    task.querySelector("p").classList.remove("completed");
+    completedTasks.splice(completedTasks.indexOf(task), 1);
+  }
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
